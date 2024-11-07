6453fec8cacf125b15f2a13f66483ed1425a318c
abijith <abijithumax100@gmail.com>
2024-11-07 15:52:16 -0500

    add edit-video page(admin)

--- routers/admin.js
+++ routers/admin.js
@@ -51,41 +51,41 @@ routes.post('/new-video', islogin, isadmin, upload.fields([{ name: 'video' }, { 
     }
 })
 
-routes.get('/all-videos',islogin, isadmin, async (req,res) => {
+routes.get('/all-videos', islogin, isadmin, async (req, res) => {
     try {
         const video = await VideoModel.find()
-        if(video.length > 0){
-            for (let index = 0; index < video.length; index++) {    
-                const title = video[index].title.substring(0,10) 
-                video[index].title = title    
+        if (video.length > 0) {
+            for (let index = 0; index < video.length; index++) {
+                const title = video[index].title.substring(0, 10)
+                video[index].title = title
             }
         }
-        res.status(200).render('admin/all-videos', {user:req.session.user,video})
+        res.status(200).render('admin/all-videos', { user: req.session.user, video })
     } catch (error) {
         console.log(error)
         res.status(400).send('Server Error')
     }
 })
 
-routes.get('/all-users', islogin, isadmin, async (req,res) => {
+routes.get('/all-users', islogin, isadmin, async (req, res) => {
     try {
         const users = await UserModel.find()
-        res.status(200).render('admin/all-users',{user:req.session.user, users})
+        res.status(200).render('admin/all-users', { user: req.session.user, users })
     } catch (error) {
         console.log(error)
         res.status(400).send('Server Error')
     }
 })
 
-routes.get('/delete-video/:id', islogin, isadmin, async (req,res) => {
+routes.get('/delete-video/:id', islogin, isadmin, async (req, res) => {
     try {
         const { id } = req.params;
         const video = await VideoModel.findById(id)
-        const title = video.title.substring(0,50)
+        const title = video.title.substring(0, 50)
         video.title = title
-        res.status(200).render('admin/delete-video',{user:req.session.user,video})
+        res.status(200).render('admin/delete-video', { user: req.session.user, video })
     } catch (error) {
-        
+
     }
 })
 
@@ -93,7 +93,7 @@ routes.post('/delete-video/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const deletedVideo = await VideoModel.findByIdAndDelete(id);
-        
+
         if (!deletedVideo) {
             return res.status(404).send('Video not found');
         }
@@ -102,6 +102,42 @@ routes.post('/delete-video/:id', async (req, res) => {
     } catch (error) {
         console.error('Error deleting video:', error);
         res.status(500).send('Server Error');
+    }
+});
+
+routes.get('/edit-page/:id', islogin, isadmin, async (req, res) => {
+    try {
+        const id = req.params.id
+        const video = await VideoModel.findById(id)
+        res.status(200).render('admin/edit-video', { user: req.session.user, video })
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('Server Error')
+    }
+})
+
+routes.post('/edit-video/:id', upload.fields([{ name: 'thumbnail' }]), async (req, res) => {
+    try {
+        const id = req.params.id;
+        const { title, description, category } = req.body;
+        
+        const thumbnailFileName = req.files['thumbnail'] ? req.files['thumbnail'][0].filename : null;
+        
+        const updateData = {
+            title,
+            description,
+            category,
+        };
+        
+        if (thumbnailFileName) {
+            updateData.img = thumbnailFileName;
+        }
+        
+        await VideoModel.findByIdAndUpdate(id, updateData);
+        res.status(200).redirect('/admin/all-videos');
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Server Error');
     }
 });
 
